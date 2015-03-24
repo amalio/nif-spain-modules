e93c4fdc27b6a3c8106f36efb5e5291e97c3bcd4
amalio <amalio@opteral.com>
2015-03-24 15:36:50 +0100

    Python module now checks full nif string
    Some refactoring too

--- python/nifspain.py
+++ python/nifspain.py
@@ -5,5 +5,18 @@ class NifSpain:
 
     correspondencia = "TRWAGMYFPDXBNJZSQVHLCKE"
 
-    def get_letter(self, nif_string):
-        return self.correspondencia[nif_string % 23]
+    def get_letter(self, nif_numbers):
+        if type(nif_numbers) is not int:
+            nif_numbers = int(nif_numbers)
+        return self.correspondencia[nif_numbers % 23]
+
+    def check(self, nif):
+        if type(nif) is not str:
+            return False
+        numbers = int(nif[:-1])
+        letter = nif[-1]
+        valid_letter = self.get_letter(numbers)
+        if valid_letter == letter:
+            return True
+        else:
+            return False
--- python/ut_nifspain.py
+++ python/ut_nifspain.py
@@ -12,9 +12,18 @@ class NifSpainTestCase(unittest.TestCase):
     def tearDown(self):
         self.nif = None
 
-    def test_giveme_correct_letter(self):
+    def test_get_letter(self):
         self.assertEqual("B", self.nif.get_letter(12332358))
         self.assertEqual("L", self.nif.get_letter(12311919))
+        self.assertEqual("B", self.nif.get_letter("12332358"))
+
+    def test_check(self):
+        self.assertTrue(self.nif.check("12332358B"))
+        self.assertTrue(self.nif.check("12311919L"))
+        self.assertFalse(self.nif.check("12332357B"))
+        self.assertFalse(self.nif.check("12"))
+        self.assertFalse(self.nif.check(12))
+
 
 
 if __name__ == '__main__':
