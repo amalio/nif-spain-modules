1318537d3cbc011ec966453f856ece3c888c134a
amalio <amalio@opteral.com>
2015-03-24 19:21:17 +0100

    Method check implemented in JavaScriptModule
    All test passes

--- javascript/nif-spain.js
+++ javascript/nif-spain.js
@@ -1,8 +1,22 @@
 var nifSpain = function() {
     var that = {};
+
     that.correspondencia = "TRWAGMYFPDXBNJZSQVHLCKE";
+
     that.getLetter = function(numbers) {
          return that.correspondencia[numbers % 23];
+    };
+
+    that.check = function(nif) {
+
+        var valid_letter;
+        if (nif.length != 9)
+            return false;
+        else {
+            valid_letter = that.getLetter(nif.substring(0, 8));
+            return valid_letter == nif[8];
+        }
+
     };
 
     return that;
