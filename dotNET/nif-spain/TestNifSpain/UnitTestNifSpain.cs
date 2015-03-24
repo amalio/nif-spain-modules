46ee693f66b6e1472f30e90b85ed64b37bf4fa1d
amalio <amalio@opteral.com>
2015-03-24 16:14:05 +0100

    Added first test in .NET library
    Triangulation needed. Updated .gitignore

---
+++ dotNET/nif-spain/TestNifSpain/UnitTestNifSpain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nif_spain;
+
+namespace TestNifSpain
+{
+    [TestClass]
+    public class UnitTestNifSpain
+    {
+        [TestMethod]
+        public void TestGetLetter()
+        {
+            NifSpain nifSpain = new NifSpain(); 
+
+            Assert.AreEqual('B', nifSpain.GetLetter(12332358));
+        }
+    }
+}
