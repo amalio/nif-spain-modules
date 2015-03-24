ea2d608a3f502880f6761a77514df960548b1ee1
amalio <amalio@opteral.com>
2015-03-24 17:24:05 +0100

    GetLetter() method implemented
    Some additional refactoring

---
+++ dotNET/nif-spain/nif-spain-test/NifSpainTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using nif_spain;
+
+namespace nif_spain_test
+{
+    [TestFixture]
+    public class NifSpainTest
+    {
+        private NifSpain _nifSpain;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _nifSpain = new NifSpain();
+        }
+
+        [Test]
+        public void TestGetLetter()
+        {
+            NifSpain nifSpain = new NifSpain();
+            Assert.AreEqual('B', _nifSpain.GetLetter(12332358));
+            Assert.AreEqual('B', _nifSpain.GetLetter("12332358"));
+            Assert.AreEqual('L', _nifSpain.GetLetter(12311919));
+        }
+        
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestGetLetterException()
+        {
+            _nifSpain.GetLetter("12332358B");
+        }
+         
+
+      
+    }
+}
