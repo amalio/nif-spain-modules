2ef2b050762e5c2b48c198b9ffdeef1908cf14bc
amalio <amalio@opteral.com>
2015-03-24 20:28:52 +0100

    Method check implemented in PHP module
    All test passes

--- php/src/NifSpain.php
+++ php/src/NifSpain.php
@@ -9,4 +9,17 @@ class NifSpain {
     {
         return $this->correspondencia[$numbers % 23];
     }
+
+    public function check($nif)
+    {
+        if (strlen($nif) != 9)
+            return false;
+        else
+        {
+            $valid_letter = $this->getLetter(substr($nif,0,9));
+
+            return $valid_letter == substr($nif,8,9);
+
+        }
+    }
 }
--- php/test/NifSpainTest.php
+++ php/test/NifSpainTest.php
@@ -3,15 +3,34 @@ require_once("../src/NifSpain.php");
 
 class NifSpainTest extends PHPUnit_Framework_TestCase {
 
+    private $nifSpain;
+
+    protected function setUp()
+    {
+        $this->nifSpain = new NifSpain();
+    }
+
+    protected function tearDown()
+    {
+       $this->nifSpain = null;
+    }
+
     public function testGetLetter()
     {
-        $nifSpain = new NifSpain();
+        $this->assertEquals("B", $this->nifSpain->getLetter(12332358));
+        $this->assertEquals("L", $this->nifSpain->getLetter(12311919));
+        $this->assertNotEquals("L", $this->nifSpain->getLetter(12311911));
+        $this->assertEquals("B", $this->nifSpain->getLetter("12332358"));
 
-        $this->assertEquals("B", $nifSpain->getLetter(12332358));
-        $this->assertEquals("L", $nifSpain->getLetter(12311919));
-        $this->assertNotEquals("L", $nifSpain->getLetter(12311911));
-        $this->assertEquals("B", $nifSpain->getLetter("12332358"));
+    }
 
+    public function testCheck()
+    {
+        $this->assertTrue($this->nifSpain->check("12332358B"));
+        $this->assertTrue($this->nifSpain->check("12311919L"));
+        $this->assertFalse($this->nifSpain->check("12311911L"));
+        $this->assertFalse($this->nifSpain->check("12"));
+        $this->assertFalse($this->nifSpain->check(12));
     }
 
 }
